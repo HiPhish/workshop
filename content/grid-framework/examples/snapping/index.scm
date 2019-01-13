975f210ed4145400598fe2a47b241d220e93df15
HiPhish <hiphish@posteo.de>
2019-01-13 13:25:28 +0100

    Add Grid Framework snapping example

---
+++ content/grid-framework/examples/snapping/index.scm
@@ -0,0 +1,12 @@
+#!/usr/bin/guile
+!#
+
+(use-modules (content grid-framework examples index))
+
+(make-example-page
+  "Runtime snapping"
+  "snapping"
+  '(p "Click and drag a block over the grid and observe how it snaps to the
+			grid's cells. The mouse input is handled by casting a ray from the cursor
+			through the camera into the grid and seeing where it hits the grid's
+			collider."))
