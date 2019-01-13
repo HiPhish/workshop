a7d32f76e98045e5c86927002783b1ad1f56d085
HiPhish <hiphish@posteo.de>
2019-01-13 11:15:07 +0100

    Add Grid Framework sliding example

---
+++ content/grid-framework/examples/sliding/index.scm
@@ -0,0 +1,15 @@
+#!/usr/bin/guile
+!#
+
+(use-modules (content grid-framework examples index))
+
+(make-example-page
+  "Sliding puzzle"
+  "sliding"
+  '(p "Unit's built-in physics system is great for 3D games with realistic
+      behaviour, but sometimes you need more basic predictable and
+      \"video-gamey\" behaviour. This example doesn't use physics at all,
+      instead it keeps track of which squares are occupied and which are free,
+      then it restricts movement accordingly by clamping the position
+      vector."))
+
