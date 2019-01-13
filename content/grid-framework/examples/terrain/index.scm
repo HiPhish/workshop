e905edd9bffc270d6e50ccb08117354c5570947c
HiPhish <hiphish@posteo.de>
2019-01-13 15:30:27 +0100

    Add Grid Framework terrain example

---
+++ content/grid-framework/examples/terrain/index.scm
@@ -0,0 +1,10 @@
+#!/usr/bin/guile
+!#
+
+(use-modules (content grid-framework examples index))
+
+(make-example-page
+  "Terrain mesh generation"
+  "terrain"
+  '(p "Left-click a vertex to raise it, right click to lower it. The text field
+      visualises the state of the terrain matrix."))
